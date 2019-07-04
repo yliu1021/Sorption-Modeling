dd5ac1c06a3a61c996f60b9be559898bf86de4f7
Yuhan Liu <yliu1021@g.ucla.edu>
2019-07-03 22:55:03 -0700

    Allows for more flexible controls via cmd line arguments

--- visualize.py
+++ visualize.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import random
+import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,25 +15,32 @@ def press(event):
         exit(0)
 
 
-v = '3'
-density_files = glob.glob('predict_mc/results{}/density*.csv'.format(v))
-grid_files = glob.glob('predict_mc/grids{}/grid*.csv'.format(v))
-density_files.sort()
-grid_files.sort()
-for density_file, grid_file in cycle(zip(density_files, grid_files)):
-    df = pd.read_csv(density_file, index_col=0)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("v", help="Show the grids/results of step v",
+                        type=int)
+    args = parser.parse_args()
+    v = args.v
+    density_files = glob.glob('generative_model/step{}/results/density*.csv'.format(v))
+    grid_files = glob.glob('generative_model/step{}/grids/grid*.csv'.format(v))
+    density_files.sort()
+    grid_files.sort()
+    for density_file, grid_file in cycle(zip(density_files, grid_files)):
+        df = pd.read_csv(density_file, index_col=0)
     
-    grid = np.genfromtxt(grid_file, delimiter=',')
+        grid = np.genfromtxt(grid_file, delimiter=',')
     
-    fig = plt.figure(1)
-    fig.canvas.mpl_connect('key_press_event', press)
+        fig = plt.figure(1)
+        fig.canvas.mpl_connect('key_press_event', press)
 
-    ax = plt.subplot(211)
-    ax.title.set_text('Grid {}'.format(grid_file))
-    ax.pcolor(grid, cmap='Greys')
-    ax.set_aspect('equal')
+        ax = plt.subplot(211)
+        ax.title.set_text('Grid {}'.format(grid_file))
+        ax.pcolor(grid, cmap='Greys')
+        ax.set_aspect('equal')
 
-    ax = plt.subplot(212)
-    ax.title.set_text('Density {}'.format(density_file))
-    ax.plot(df.index[0:N_ADSORP], df['0'][0:N_ADSORP])
-    plt.show()
+        ax = plt.subplot(212)
+        ax.title.set_text('Density {}'.format(density_file))
+        ax.plot(df.index[0:N_ADSORP], df['0'][0:N_ADSORP])
+        ax.plot(np.linspace(0, N_ADSORP, num=N_ADSORP), np.linspace(0, 1, num=N_ADSORP))
+        ax.set_aspect(N_ADSORP)
+        plt.show()
