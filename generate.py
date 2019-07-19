0b5de08667040c484eb3c1e048320a066c686b76
Yuhan Liu <yliu1021@g.ucla.edu>
2019-07-19 00:44:00 -0700

    Fixed model freezing bug

--- generate.py
+++ generate.py
@@ -54,13 +54,13 @@ uniform_boost_dim = 1
 loss_weights = [1, 0.05] # weights of losses in the metric and each latent code
 
 proxy_enforcer_epochs = 20
-# proxy_enforcer_epochs = 2
+proxy_enforcer_epochs = 5
 proxy_enforcer_batchsize = 64
 
 generator_train_size = 10000
 # generator_train_size = 128
 generator_epochs = 20
-# generator_epochs = 1
+generator_epochs = 2
 generator_batchsize = 64
 generator_train_size //= generator_batchsize
 
@@ -117,19 +117,19 @@ def make_proxy_enforcer_model():
     x = Cropping2D(cropping=(GRID_SIZE - (first_filter_size - 1), GRID_SIZE - (first_filter_size - 1)))(x)
 
     x = Conv2D(16, first_filter_size, padding='valid', name='conv0')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     
     x = Conv2D(32, 3, padding='valid', name='conv1')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Conv2D(64, 3, padding='valid', name='conv2')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Conv2D(128, 3, padding='valid', strides=2, name='conv3')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     
     x = Conv2D(256, 3, padding='valid', strides=2, name='conv4')(x)   
@@ -162,41 +162,41 @@ def make_generator_model():
     H_GRID_SIZE = GRID_SIZE // 2
 
     x = Dense(Q_GRID_SIZE * Q_GRID_SIZE * 8, name='fc1')(conc)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Dense(Q_GRID_SIZE * Q_GRID_SIZE * 8, name='fc3')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Dense(Q_GRID_SIZE * Q_GRID_SIZE * 8, name='fc4')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Dense(Q_GRID_SIZE * Q_GRID_SIZE * 16, name='fc5')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Reshape((Q_GRID_SIZE, Q_GRID_SIZE, 16))(x)
 
     x = Conv2DTranspose(16, 2, strides=1, padding='same', name='deconv1')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     
     x = Conv2DTranspose(32, 3, strides=1, padding='same', name='deconv2')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     
     x = Conv2DTranspose(32, 3, strides=2, padding='same', name='deconv_expand_1')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     x = Conv2DTranspose(128, 3, strides=2, padding='same', name='deconv_expand_2')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     
     x = Conv2DTranspose(128, 3, strides=1, padding='same', name='deconv_3')(x)
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = LeakyReLU()(x)
 
     out = Conv2D(1, 1, strides=1, padding='same', activation=binary_sigmoid, name='generator_conv')(x)
@@ -237,6 +237,22 @@ def make_generator_input(n_grids, use_generator=False, batchsize=generator_batch
         return artificial_metrics, uniform_latent_code
 
 
+# def print_weight(model):
+#     layer = model.layers[4]
+#     weights = layer.get_weights()
+#     print(weights)
+
+
+def freeze_model(model):
+    for layer in model.layers:
+        layer.trainable = False
+
+
+def unfreeze_model(model):
+    for layer in model.layers:
+        layer.trainable = True
+
+
 def train_step(generator_model, proxy_enforcer_model, lc_uni, step):
     """
     1) Train M on the grids in generative_models_*
@@ -258,7 +274,7 @@ def train_step(generator_model, proxy_enforcer_model, lc_uni, step):
     
     # Train M
     # load the grids and density diffs
-    proxy_enforcer_model.trainable = True
+    unfreeze_model(proxy_enforcer_model)
     optimizer = Adam(lr=0.001, clipnorm=1.0)
     proxy_enforcer_model.compile(optimizer, loss='kullback_leibler_divergence', metrics=['mae', worst_abs_loss])
     summarize_model(proxy_enforcer_model)
@@ -279,9 +295,11 @@ def train_step(generator_model, proxy_enforcer_model, lc_uni, step):
 
     latent_code_uni = Input(shape=(uniform_boost_dim,), name='latent_code')
     inp = Input(shape=(N_ADSORP,), name='target_metric')
+    
+    generator_model.compile('adam', loss='mse', metrics=['mae'])
     generator_out = generator_model([inp, latent_code_uni])
     
-    proxy_enforcer_model.trainable = False
+    freeze_model(proxy_enforcer_model)
     proxy_enforcer_out = proxy_enforcer_model(generator_out)
     
     latent_code_uni_out = lc_uni(generator_out)
@@ -290,7 +308,7 @@ def train_step(generator_model, proxy_enforcer_model, lc_uni, step):
                            outputs=[proxy_enforcer_out, latent_code_uni_out])
 
     optimizer = Adam(lr=0.001, clipnorm=1.0)
-    training_model.compile(optimizer, loss=['poisson', 'mse'],
+    training_model.compile(optimizer, loss=['kullback_leibler_divergence', 'mse'],
                            metrics={
                                'proxy_enforcer_model': ['mae', worst_abs_loss],
                                'uniform_latent_code_model': 'mae',
@@ -332,6 +350,48 @@ def train_step(generator_model, proxy_enforcer_model, lc_uni, step):
         path = os.path.join(artificial_metrics_dir, 'artificial_metric_%04d.csv'%i)
         np.savetxt(path, artificial_metric, fmt='%f', delimiter=',')
 
+    # Visualize
+    step_dir = os.path.join(base_dir, 'step{}'.format(step))
+    grid_dir = os.path.join(step_dir, 'grids')
+    density_dir = os.path.join(step_dir, 'results')
+    artificial_metrics_dir = os.path.join(step_dir, 'artificial_metrics')
+    
+    grid_files = glob.glob(os.path.join(grid_dir, 'grid_*'))
+    grid_files.sort()
+    density_files = glob.glob(os.path.join(density_dir, 'density_*'))
+    density_files.sort()
+    artificial_metrics_files = glob.glob(os.path.join(artificial_metrics_dir, 'artificial_metric_*'))
+    artificial_metrics_files.sort()
+    
+    for grid_file, density_file, artificial_metric_file in zip(grid_files, density_files, artificial_metrics_files):
+        grid = np.genfromtxt(grid_file, delimiter=',')
+        density = np.genfromtxt(density_file, delimiter=',', skip_header=1, max_rows=N_ADSORP)
+        density = density[:, 1]
+        artificial_metrics = np.genfromtxt(artificial_metric_file, delimiter=',')
+        
+        pred_diff = proxy_enforcer_model.predict(np.array([grid]))[0]
+        pred_density = np.cumsum(pred_diff)
+        artificial_density = np.cumsum(artificial_metrics)
+        
+        fig = plt.figure(1, figsize=(6, 8))
+        fig.canvas.mpl_connect('key_press_event', press)
+
+        ax = plt.subplot(211)
+        ax.pcolor(grid, cmap='Greys')
+        ax.set_aspect('equal')
+
+        ax = plt.subplot(212)
+        x = np.linspace(0, 1, N_ADSORP+1)
+        ax.plot(x, np.insert(density, N_ADSORP, 1))
+        ax.plot(x, np.insert(artificial_density, 0, 0))
+        ax.plot(x, np.insert(pred_density, 0, 0))
+        ax.legend(['Actual', 'Target for M^-1', 'Predicted by M'], loc='best')
+        # ax.set_aspect('equal')
+
+        metric = np.mean(np.abs(artificial_density - pred_density))
+        fig.text(0.5, 0.05, 'Mean absolute difference: {:.4f}'.format(metric), ha='center')
+
+        plt.show()
 
 def visualize_enforcer(model_step=None):
     if model_step is None:
@@ -406,8 +466,10 @@ def visualize_enforcer(model_step=None):
 def visualize_generator(step, model_step=None):
     if model_step is None:
         model_step = step
-    enforcer_model = load_model(os.path.join(base_dir, 'step{}/enforcer.hdf5'.format(model_step)),
-                                custom_objects={'worst_abs_loss': worst_abs_loss})
+    enforcer_model, _ = make_proxy_enforcer_model()
+    enforcer_model.load_weights(os.path.join(base_dir, 'step{}/enforcer.hdf5'.format(model_step)))
+    # enforcer_model = load_model(os.path.join(base_dir, 'step{}/enforcer.hdf5'.format(model_step)),
+    #                             custom_objects={'worst_abs_loss': worst_abs_loss})
     step_dir = os.path.join(base_dir, 'step{}'.format(step))
     grid_dir = os.path.join(step_dir, 'grids')
     density_dir = os.path.join(step_dir, 'results')
