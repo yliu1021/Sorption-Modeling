52125bf07656af9202b12c66b3bc220dc7a080c9
Kevin Li <kevinli120@yahoo.com>
2019-07-21 12:28:56 -0700

    Switch order of reproduction and natural selection

--- cpp/src/evolution.cpp
+++ cpp/src/evolution.cpp
@@ -16,30 +16,27 @@ using namespace std;
 #include "constants.h"
 #include "helpers.h"
 
-// TODO: Kill grids before reproducing
 // TODO: Multithread
 
-constexpr int WORKERS = 200;
+constexpr int WORKERS = 1000;
 constexpr int ITERS = 10;
 constexpr double MUT_RATE = 0.6; // Mutation rate
 
 int main(int argc, char *argv[]) {
-	setup_NL(); // for fast_dft
-
-    vector<array<double,N_SQUARES>> grids;
 
     if (argc == 2) {
-        string grid_path(argv[1]);
-        array<double, N_SQUARES> grid = load_grid(grid_path);
-        array<double, N_ADSORP+1> linear = linear_curve();
-        vector<double> costs;
-        double grid_cost = mean_abs_error(linear, run_dft(grid));
-        for (int i = 0; i < WORKERS; ++i) {
-            grids.push_back(grid);
-            costs.push_back(grid_cost);
-        }
 
-        srand(time(NULL));
+        setup_NL(); // for fast_dft
+
+        string grid_path(argv[1]);
+        array<double, N_SQUARES> start_grid = load_grid(grid_path);
+        array<double, N_ADSORP+1> lin_curve = linear_curve();
+        double grid_cost = mean_abs_error(lin_curve, run_dft(start_grid));
+
+        vector<array<double,N_SQUARES>> grids(WORKERS, start_grid);
+        vector<double> costs(WORKERS, grid_cost);
+
+        srand(time(NULL)); // Generate random seed for random number generation
 
         // double min_cost = *min_element(costs.begin(), costs.end());
 
@@ -48,10 +45,11 @@ int main(int argc, char *argv[]) {
             for (int j = 0; j < WORKERS; ++j) {
                 if (((double)rand()/(RAND_MAX)) < MUT_RATE) {
                     toggle_random(grids[j]);
-                    costs[j] = mean_abs_error(linear, run_dft(grids[j]));
+                    costs[j] = mean_abs_error(lin_curve, run_dft(grids[j]));
                 }
             }
             if (i == (ITERS - 1)) { break; }
+
             double min_cost_iter = *min_element(costs.begin(), costs.end());
             cout << "Minimum cost for iteration " << i << ": " << min_cost_iter << endl;
             // if (min_cost_iter < min_cost) {
@@ -64,33 +62,33 @@ int main(int argc, char *argv[]) {
             //     }
             //     cout << endl;
             // }
+
             vector<double> norm_costs(costs);
-            normalizeVec(costs);
-            // normalizeArr(norm_costs.begin(), norm_costs.end(), *min_element(norm_costs.begin(), norm_costs.end()), *max_element(norm_costs.begin(), norm_costs.end()));
-
-            // Reproduce suitable grids
-            for (int j = 0; j < WORKERS; ++j) {
-                 if (((double)rand()/(RAND_MAX)) < (1-norm_costs[j])) {
-                    array<double,N_SQUARES> copy_grid(grids[j]);
-                    grids.push_back(copy_grid);
-                    costs.push_back(costs[j]);
-                    norm_costs.push_back(norm_costs[j]);
-                }
-            }
-
-            cout << "After reproduction: " << grids.size() << " grids alive" << endl;
+            normalizeVec(norm_costs);
 
             // Kill unsuitable grids
-            while (grids.size() > WORKERS) {
-                int rand_grid = rand() % grids.size();
-                if (((double)rand()/(RAND_MAX)) < norm_costs[rand_grid]) {
-                    grids.erase(grids.begin()+rand_grid);
-                    norm_costs.erase(norm_costs.begin()+rand_grid);
-                    costs.erase(costs.begin()+rand_grid);
+            for (int j = WORKERS-1; j >= 0; --j) {
+                if (((double)rand()/(RAND_MAX)) < norm_costs[j]) {
+                    grids.erase(grids.begin()+j);
+                    norm_costs.erase(norm_costs.begin()+j);
+                    costs.erase(costs.begin()+j);
                 }
             }
 
             cout << "After natural selection: " << grids.size() << " grids alive" << endl;
+
+            // Reproduce suitable grids
+            while (grids.size() < WORKERS) {
+                int rand_grid = rand() % grids.size();
+                if (((double)rand()/(RAND_MAX)) < (1-norm_costs[rand_grid])) {
+                    array<double,N_SQUARES> copy_grid(grids[rand_grid]);
+                    grids.push_back(copy_grid);
+                    costs.push_back(costs[rand_grid]);
+                    norm_costs.push_back(norm_costs[rand_grid]);
+                }
+            }
+
+            cout << "After reproduction: " << grids.size() << " grids alive" << endl;
 
             // string grid_file = grid_path + "optimized.csv"
 
