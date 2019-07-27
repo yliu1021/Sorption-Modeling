0ec32ef8af0a03b65a94d894c6b02d3ccd6c0f9d
Yuhan Liu <yliu1021@g.ucla.edu>
2019-07-27 00:51:40 -0700

    hyperparameter tuning

--- bayesian_optimizer.py
+++ bayesian_optimizer.py
@@ -136,7 +136,7 @@ def minimize(parameters):
     return result
 
 
-res = gp_minimize(minimize, parameter_bounds, n_calls=30,
+res = gp_minimize(minimize, parameter_bounds, n_calls=25,
                   verbose=True)
 print(all_hyperparameter_keys)
 print(res.x)
