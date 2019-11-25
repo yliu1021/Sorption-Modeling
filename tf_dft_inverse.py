9344536ec93a08447be290796f049ec61467d138
Yuhan Liu <yliu1021@g.ucla.edu>
2019-11-24 23:40:45 -0800

    Deeper generator

--- tf_dft_inverse.py
+++ tf_dft_inverse.py
@@ -100,15 +100,15 @@ def inverse_dft_model():
 
     Q_GRID_SIZE = GRID_SIZE // 4
 
-    x = Dense(GRID_SIZE * GRID_SIZE * 64, name='fc1')(inp)
+    x = Dense(GRID_SIZE * GRID_SIZE * 128, name='fc1')(inp)
     x = LeakyReLU()(x)
 
-    x = Reshape((GRID_SIZE, GRID_SIZE, 64))(x)
+    x = Reshape((GRID_SIZE, GRID_SIZE, 128))(x)
 
-    x = Conv2DTranspose(64, 20, strides=1, padding='same')(x)
+    x = Conv2DTranspose(128, 10, strides=1, padding='same')(x)
     x = LeakyReLU()(x)
 
-    x = Conv2DTranspose(64, 20, strides=1, padding='same')(x)
+    x = Conv2DTranspose(64, 10, strides=1, padding='same')(x)
     x = LeakyReLU()(x)
 
     out = Conv2D(1, 1, strides=1, padding='same', activation=binary_sigmoid, name='generator_conv')(x)
