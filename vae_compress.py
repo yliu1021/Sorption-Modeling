893e3f7fa5ec56685e0b68361281afbcf24ae661
Kevin Li <kevinli120@yahoo.com>
2020-01-06 15:14:32 -0800

    Implement compression with conv

--- vae_compress.py
+++ vae_compress.py
@@ -49,21 +49,34 @@ original_dim = GRID_SIZE * GRID_SIZE
 # x_train, y_train = data.get_all_data(matching='vae_model_0')
 x_train, y_train = data.get_all_data(matching='generative_model_2')
 
-pdb.set_trace()
-
-x_train = np.reshape(x_train, [-1, original_dim])
+x_train = np.reshape(x_train, [-1, GRID_SIZE, GRID_SIZE, 1])
 
 # network parameters
-input_shape = (original_dim, )
-intermediate_dim = 512
+input_shape = (GRID_SIZE, GRID_SIZE, 1)
 batch_size = 128
-latent_dim = 5
-epochs = 50
+kernel_size = 3
+filters = 16
+latent_dim = 2
+epochs = 30
 
 # VAE model = encoder + decoder
 # build encoder model
 inputs = Input(shape=input_shape, name='encoder_input')
-x = Dense(intermediate_dim, activation='relu')(inputs)
+x = inputs
+for i in range(2):
+    filters *= 2
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               activation='relu',
+               strides=2,
+               padding='same')(x)
+
+# shape info needed to build decoder model
+shape = K.int_shape(x)
+
+# generate latent vector Q(z|X)
+x = Flatten()(x)
+x = Dense(16, activation='relu')(x)
 z_mean = Dense(latent_dim, name='z_mean')(x)
 z_log_var = Dense(latent_dim, name='z_log_var')(x)
 
@@ -76,39 +89,44 @@ encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
 
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
+x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+for i in range(2):
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
+                        activation='relu',
+                        strides=2,
+                        padding='same')(x)
+    filters //= 2
+
+outputs = Conv2DTranspose(filters=1,
+                          kernel_size=kernel_size,
+                          activation='sigmoid',
+                          padding='same',
+                          name='decoder_output')(x)
 
 # instantiate decoder model
 decoder = Model(latent_inputs, outputs, name='decoder')
 
 # instantiate VAE model
 outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae_mlp')
+vae = Model(inputs, outputs, name='vae')
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # help_ = "Load h5 model trained weights"
-    # parser.add_argument("-w", "--weights", help=help_)
-    # help_ = "Use mse loss instead of binary cross entropy (default)"
-    # parser.add_argument("-m",
-    #                     "--mse",
-    #                     help=help_, action='store_true')
-    # args = parser.parse_args()
     models = (encoder, decoder)
+    # data = (x_test, y_test)
 
     # VAE loss = mse_loss or xent_loss + kl_loss
-    reconstruction_loss = binary_crossentropy(inputs, outputs)
+    reconstruction_loss = binary_crossentropy(K.flatten(inputs), K.flatten(outputs))
 
-    reconstruction_loss *= original_dim
+    reconstruction_loss *= GRID_SIZE * GRID_SIZE
     kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
     kl_loss = K.sum(kl_loss, axis=-1)
     kl_loss *= -0.5
     vae_loss = K.mean(reconstruction_loss + kl_loss)
     vae.add_loss(vae_loss)
-    optimizer = Adam(10**-2)
-    vae.compile(optimizer=optimizer)
+    vae.compile(optimizer='rmsprop')
 
     # train the autoencoder
     vae.fit(x_train, epochs=epochs, batch_size=batch_size)
-    # vae.save_weights('vae_mlp_mnist.h5')
