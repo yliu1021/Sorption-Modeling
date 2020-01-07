cfe14e77b37e328697a12c54e8f790e1497bffa0
Kevin Li <kevinli120@yahoo.com>
2020-01-06 21:46:53 -0800

    verify that distributions work

--- vae_compress.py
+++ vae_compress.py
@@ -49,7 +49,14 @@ original_dim = GRID_SIZE * GRID_SIZE
 # x_train, y_train = data.get_all_data(matching='vae_model_0')
 x_train, y_train = data.get_all_data(matching='generative_model_2')
 
+num_test = 2000
+x_test = x_train[:num_test]
+y_test = y_train[:num_test]
+x_train = x_train[num_test:]
+y_train = y_train[num_test:]
+
 x_train = np.reshape(x_train, [-1, GRID_SIZE, GRID_SIZE, 1])
+x_test = np.reshape(x_test, [-1, GRID_SIZE, GRID_SIZE, 1])
 
 # network parameters
 input_shape = (GRID_SIZE, GRID_SIZE, 1)
@@ -57,7 +64,7 @@ batch_size = 128
 kernel_size = 3
 filters = 16
 latent_dim = 2
-epochs = 30
+epochs = 15
 
 # VAE model = encoder + decoder
 # build encoder model
@@ -113,9 +120,85 @@ decoder = Model(latent_inputs, outputs, name='decoder')
 outputs = decoder(encoder(inputs)[2])
 vae = Model(inputs, outputs, name='vae')
 
+def plot_results(models,
+                 data,
+                 batch_size=128,
+                 model_name="vae_mnist"):
+    """Plots labels and MNIST digits as function of 2-dim latent vector
+
+    # Arguments
+        models (tuple): encoder and decoder models
+        data (tuple): test data and label
+        batch_size (int): prediction batch size
+        model_name (string): which model is using this function
+    """
+
+    encoder, decoder = models
+    x_test, y_test = data
+    os.makedirs(model_name, exist_ok=True)
+
+    filename = os.path.join(model_name, "vae_mean.png")
+    # display a 2D plot of the digit classes in the latent space
+    z_mean, _, _ = encoder.predict(x_test,
+                                   batch_size=batch_size)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(z_mean[:, 0], z_mean[:, 1])
+    plt.colorbar()
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.savefig(filename)
+    plt.show()
+
+    filename = os.path.join(model_name, "digits_over_latent.png")
+    # display a 30x30 2D manifold of digits
+    n = 30
+    digit_size = 20 
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-4, 4, n)
+    grid_y = np.linspace(-4, 4, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = decoder.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+
+            plt.figure(figsize=(10,10))
+            plt.pcolor(digit, cmap='Greys_r', vmin=0.0, vmax=1.0)
+            plt.title('{}, {}'.format(xi, yi))
+            plt.show()
+
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+
+    plt.figure(figsize=(10, 10))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range + 1
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.imshow(figure, cmap='Greys_r')
+    plt.savefig(filename)
+    plt.show()
+
+    randoms = [np.random.normal(0, 1, 2) for _ in range(10)]
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    help_ = "Load h5 model trained weights"
+    parser.add_argument("-w", "--weights", help=help_)
+    help_ = "Use mse loss instead of binary cross entropy (default)"
+    parser.add_argument("-m", "--mse", help=help_, action='store_true')
+    args = parser.parse_args()
+
     models = (encoder, decoder)
-    # data = (x_test, y_test)
+    data = (x_train, y_train)
 
     # VAE loss = mse_loss or xent_loss + kl_loss
     reconstruction_loss = binary_crossentropy(K.flatten(inputs), K.flatten(outputs))
@@ -128,5 +211,11 @@ if __name__ == '__main__':
     vae.add_loss(vae_loss)
     vae.compile(optimizer='rmsprop')
 
-    # train the autoencoder
-    vae.fit(x_train, epochs=epochs, batch_size=batch_size)
+    if args.weights:
+        vae.load_weights(args.weights)
+    else:
+        # train the autoencoder
+        vae.fit(x_train, epochs=epochs, batch_size=batch_size)
+        # vae.save_weights('vae_cnn_mnist.h5')
+
+    plot_results(models, (x_test, y_test), batch_size=batch_size, model_name="vae_cnn")
