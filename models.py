436d9f3e0b9163ae68e255240e65e19b0a4494a6
Kevin Li <kevinli120@yahoo.com>
2020-01-11 11:07:58 -0800

    Restructure files

--- models.py
+++ models.py
@@ -34,22 +34,6 @@ def unfreeze(model):
     for layer in model.layers:
         layer.trainable = True
 
-def sampling(args):
-    """Reparameterization trick by sampling from an isotropic unit Gaussian.
-
-    # Arguments
-        args (tensor): mean and log of variance of Q(z|X)
-
-    # Returns
-        z (tensor): sampled latent vector
-    """
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    # by default, random_normal has mean = 0 and std = 1.0
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
-
 # Predictor model
 def make_predictor_model(**kwargs):
     first_filter_size = kwargs.get('first_filter_size', 3)
@@ -145,104 +129,6 @@ def make_generator_model(**kwargs):
     out = Reshape((GRID_SIZE, GRID_SIZE))(out)
 
     model = Model(inputs=[inp, latent_code_inp], outputs=[out],
-                  name='generator_model')
-    
-    return model
-
-def make_vae_encoder(**kwargs):
-    first_filter_size = kwargs.get('first_filter_size', 3)
-    last_conv_depth = kwargs.get('last_conv_depth', 256)
-    dense_layer_size = kwargs.get('dense_layer_size', 2048)
-    boost_dim = kwargs.get('boost_dim', 5)
-    num_convs = kwargs.get('num_convs', 1)
-    boundary_expand = kwargs.get('boundary_expand', 4)
-
-    inp = Input(shape=(GRID_SIZE, GRID_SIZE), name='proxy_enforcer_input')
-    x = Lambda(lambda x: K.tile(x, [1, 3, 3]))(inp)
-    x = Reshape((GRID_SIZE * 3, GRID_SIZE * 3, 1))(x)
-    x = Cropping2D(cropping=(GRID_SIZE-boundary_expand, GRID_SIZE-boundary_expand))(x)
-
-    x = Conv2D(16, first_filter_size, padding='valid', name='preconv')(x)
-    for i in range(1, num_convs+1):
-        x = Conv2D(32, 3, padding='valid', name='preconv{}'.format(i))(x)
-    x = Conv2D(64, 3, padding='valid', name='preconv_final')(x)
-    x = Dropout(0.1)(x)
-    
-    x = Conv2D(128, 3, padding='valid', strides=2, name='conv_stride_1')(x)
-    x = LeakyReLU()(x)
-    
-    x = Conv2D(last_conv_depth, 3, padding='valid', strides=2, name='conv_stride_2')(x)   
-    x = LeakyReLU()(x)
-
-    x = Flatten()(x)
-    x = Dropout(0.1)(x)
-
-    x_fc1 = Dense(dense_layer_size, name='hidden_fc_1', activation='relu')(x)
-    x_fc1 = Dropout(0.5)(x_fc1)
-    hidden = Dense(dense_layer_size, name='hidden_fc_final', activation='relu')(x_fc1)
-    hidden = Dropout(0.5)(hidden)
-
-    z_mean = Dense(boost_dim, name='z_mean')(x_fc1)
-    z_log_var = Dense(boost_dim, name='z_log_var')(x_fc1)
-
-    z = Lambda(sampling, output_shape=(boost_dim,), name='z')([z_mean, z_log_var])
-
-    encoder = Model(inp, [z_mean, z_log_var, z], name='encoder')
-
-    return encoder
-    
-def make_vae_decoder(**kwargs):
-    first_conv_depth = kwargs.get('first_conv_depth', 128)
-    pre_deconv1_depth = kwargs.get('pre_deconv1_depth', 128)
-    post_deconv2_depth = kwargs.get('post_deconv2_depth', 32)
-    last_filter_size = kwargs.get('last_filter_size', 6)
-    boost_dim = kwargs.get('boost_dim', 5)
-
-    lc_merge_depth = 128
-
-    latent_inputs = Input(shape=(boost_dim,), name='z_sampling')
-    latent_code = Dense(512, use_bias=True, activation='relu')(latent_inputs)
-    latent_code = Dense(lc_merge_depth, use_bias=False)(latent_code)
-
-    inp = Input(shape=(N_ADSORP,))
-
-    Q_GRID_SIZE = GRID_SIZE // 4
-
-    x = Dense(2048, name='fc1')(inp)
-
-    x = Dense(2048, name='fc2')(x)
-    x = LeakyReLU()(x)
-
-    x = Dense(Q_GRID_SIZE * Q_GRID_SIZE * first_conv_depth, name='fc3')(x)
-    x = LeakyReLU()(x)
-
-    x = Reshape((Q_GRID_SIZE, Q_GRID_SIZE, first_conv_depth))(x)
-
-    x = Conv2DTranspose(first_conv_depth, 5, strides=1, padding='same', name='pre_deconv1')(x)
-    x = LeakyReLU()(x)
-    
-    x = Conv2DTranspose(pre_deconv1_depth, 3, strides=1, padding='same', name='pre_deconv2')(x)
-    x = LeakyReLU()(x)
-
-    x = Conv2DTranspose(lc_merge_depth, 3, strides=2, padding='same', name='deconv_expand1')(x)
-    x = LeakyReLU()(x)
-
-    x = Multiply()([x, latent_code])
-
-    x = Conv2DTranspose(64, 3, strides=2, padding='same', name='deconv_expand2')(x)
-    x = LeakyReLU()(x)
-
-    x = Conv2DTranspose(post_deconv2_depth, 3, strides=1, padding='same', name='post_deconv')(x)
-    x = LeakyReLU()(x)
-    
-    x = Conv2D(32, 3, strides=1, padding='same', name='smooth_conv')(x)
-    x = LeakyReLU()(x)
-    
-    out = Conv2D(1, last_filter_size, strides=1, padding='same',
-                 activation=binary_sigmoid, name='generator_conv')(x)
-    out = Reshape((GRID_SIZE, GRID_SIZE))(out)
-
-    model = Model(inputs=[inp, latent_inputs], outputs=[out],
                   name='generator_model')
     
     return model
---
+++ vae/visualize_vae.py
@@ -0,0 +1,217 @@
+import sys
+sys.path.append('..')
+
+import os
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# from itertools import cycle
+import imageio
+from tensorflow.keras.models import load_model
+
+import data
+from constants import *
+from simul_dft import *
+
+model_name = 'vae_deconv'
+
+# # Linear curve
+target_density = np.arange(41) * STEP_SIZE
+
+# # Heaviside step function
+# target_density = np.arange(41) * STEP_SIZE # heaviside
+# target_density = target_density - 0.5 # heaviside
+# target_density = np.heaviside(target_density, 0.5)
+
+# # Multi-step function
+# target_density = np.arange(41) * STEP_SIZE
+# target_density = np.piecewise(target_density, [target_density<=0.3, np.logical_and(target_density>0.3, target_density<=0.6), target_density>0.6], [0.3, 0.6, 1])
+
+# Circle functions
+# target_density = np.genfromtxt("../../../../Desktop/circle_up.csv", delimiter=",")
+# target_density = np.genfromtxt("../../../../Desktop/circle_down.csv", delimiter=",")
+
+# target_density = np.genfromtxt("../../../../Desktop/1pore_den.csv", delimiter=",")
+# target_density = np.genfromtxt("../../../../Desktop/1solid_den.csv", delimiter=",")
+# target_density = np.genfromtxt("../../../../Desktop/bigpore.csv", delimiter=",")
+
+def press(event):
+    if event.key != 'q':
+        exit(0)
+
+# def make_gif():
+#     images = []
+#     for filename in sorted(os.listdir('evol_animation')):
+#         if filename[0] == 'g': # if grid
+#                 images.append(imageio.imread('evol_animation/'+filename))
+#     imageio.mimsave('evol_animation/animated_evol.gif', images, duration=0.1)
+
+def plot_latent(models, test_data, batch_size=128, use_curve=True, save=False):
+    """
+    Graphs of the 2-d latent vector. Untested with higher dimensions.
+    Currently shows with a linear target curve.
+    """
+    encoder, decoder = models
+    x_test, y_test = test_data
+
+    os.makedirs(model_name+'/grids', exist_ok=True)
+    filename = os.path.join(model_name, 'vae_mean.png')
+
+    # display a 2D plot of the digit classes in the latent space
+    z_mean, _, _ = encoder.predict(x_test, batch_size=batch_size)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(z_mean[:, 0], z_mean[:, 1])
+    plt.xlabel('z[0]')
+    plt.ylabel('z[1]')
+    plt.title('Latent Encoding of Test Grids')
+    if save:
+        plt.savefig(filename)
+    plt.show()
+
+    # display a 30x30 2D manifold of grids
+    filename = os.path.join(model_name, 'grids_over_latent.png')
+    n = 30
+    figure = np.zeros((GRID_SIZE * n, GRID_SIZE * n))
+    # linearly spaced coordinates corresponding to the 2D plot of digit classes in the latent space
+    grid_x = np.linspace(-4, 4, n)
+    grid_y = np.linspace(-4, 4, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            if use_curve:
+                x_decoded = decoder.predict([z_sample, target_density])
+            else:
+                x_decoded = decoder.predict(z_sample)
+            digit = x_decoded[0].reshape(GRID_SIZE, GRID_SIZE)
+
+            path = os.path.join(model_name, 'grids', 'grid_{:04d}.csv'.format(i*n+j))
+            np.savetxt(path, digit, fmt='%i', delimiter=',')
+
+            # plt.figure(figsize=(10,10))
+            # plt.pcolor(digit, cmap='Greys_r', vmin=0.0, vmax=1.0)
+            # plt.title('{}, {}'.format(xi, yi))
+            # plt.show()
+
+            figure[i * GRID_SIZE: (i + 1) * GRID_SIZE,
+                   j * GRID_SIZE: (j + 1) * GRID_SIZE] = digit
+
+    plt.figure(figsize=(10, 10))
+    start_range = GRID_SIZE // 2
+    end_range = n * GRID_SIZE + start_range + 1
+    pixel_range = np.arange(start_range, end_range, GRID_SIZE)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel('z[0]')
+    plt.ylabel('z[1]')
+    plt.imshow(figure, cmap='Greys_r')
+    plt.title('Grids Over Latent Distribution')
+    if save:
+        plt.savefig(filename)
+    plt.show()
+
+
+def show_grids():
+    density_files = glob.glob(os.path.join(base_dir, 'density*.csv'))
+    grid_files = glob.glob(os.path.join(base_dir, 'grid*.csv'))
+    density_files.sort()
+    grid_files.sort()
+    for density_file, grid_file in zip(density_files, grid_files):
+        df = pd.read_csv(density_file, index_col=0)
+        density = df['0'][0:N_ADSORP+1]
+
+        grid = np.genfromtxt(grid_file, delimiter=',')
+        grid = grid[:, :20]
+        # density = run_dft_fast(np.reshape(grid, 400))
+
+        metric = (np.sum(np.absolute(density - target_density)) / 20.0)
+    
+        fig = plt.figure(1, figsize=(6, 8))
+        fig.canvas.mpl_connect('key_press_event', press)
+        fig.suptitle('{}, {}'.format('/'.join(grid_file.split('/')[-3:]), '/'.join(density_file.split('/')[-3:])))
+
+        ax = plt.subplot(211)
+        ax.pcolor(grid, cmap='Greys_r', vmin=0.0, vmax=1.0)
+        ax.set_aspect('equal')
+
+        ax = plt.subplot(212)
+        ax.plot(df.index[0:N_ADSORP+1], df['0'][0:N_ADSORP+1])
+        ax.plot(np.linspace(0, N_ADSORP+1, num=N_ADSORP+1), target_density)
+        plt.plot([1],[1])
+        ax.legend(['Metric: {:.4f}'.format(metric), 'Target'])
+        ax.set_aspect(N_ADSORP+1)
+        
+        plt.savefig('evol_animation/' + grid_file[-13:-4] + '.png')
+        plt.close()
+
+        plt.show()
+
+
+def show_grids_no_density():
+
+    base_dir = 'vae_cnn_1'
+
+    grid_files = glob.glob(os.path.join(base_dir, 'grid*.csv'))
+    grid_files.sort()
+    for i, grid_file in enumerate(grid_files):
+        grid = np.genfromtxt(grid_file, delimiter=',')
+        grid = grid[:, :20]
+        density = run_dft_fast(np.reshape(grid, 400))
+        density = density[:41]
+        density[40] = 1
+
+        metric = (np.sum(np.absolute(density - target_density)) / 20.0)
+
+        fig = plt.figure(figsize=(10, 4))
+        fig.canvas.mpl_connect('key_press_event', press)
+        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+        fig.suptitle('Grid {}'.format(i))
+
+        ax = plt.subplot(1, 2, 1)
+        ax.clear()
+        ax.set_title('Grid (Black = Solid, White = Pore)')
+        ax.set_yticks(np.linspace(0, 20, 5))
+        ax.set_xticks(np.linspace(0, 20, 5))
+        ax.pcolor(grid, cmap='Greys_r', vmin=0.0, vmax=1.0)
+        ax.set_aspect('equal')
+
+        ax = plt.subplot(1, 2, 2)
+        ax.clear()
+        ax.set_title('Adsorption Curve')
+        relative_humidity = np.arange(41) * STEP_SIZE
+        ax.plot(relative_humidity, density, label='DFT')
+        if target_density is not None:
+            ax.plot(relative_humidity, target_density, label='Target')
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.set_xlabel('Relative Humidity')
+        ax.set_ylabel('Proportion of Pores filled')
+        ax.set_aspect('equal')
+        ax.legend()
+       
+        # # plt.savefig('visualizations/' + grid_file[-13:-4] + '.png')
+        # # plt.close()
+
+        plt.show() 
+        plt.close()
+
+# show_grids_no_density()
+
+if __name__ == '__main__':
+    encoder = load_model(model_name+'/encoder.tf')
+    decoder = load_model(model_name+'/decoder.tf')
+    x_test, y_test = data.get_all_data(matching='vae_cnn_data')
+    # x_test, y_test = data.get_all_data(matching='../generative_model_2')
+    # x_test = x_test[:100]
+    x_test = np.reshape(x_test, [-1, GRID_SIZE, GRID_SIZE, 1])
+    # y_test = y_test[:100]
+    plot_latent((encoder, decoder), (x_test, y_test), use_curve=False, save=True)
+
+
+# print('creating images')
+# show_grids_no_density()
+# print('creating animation')
+# make_gif()
